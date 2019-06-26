54d4ffa829302a5b03263bb5e10c0905c7789b3d
Владислав Пархоменко <parhom895@gmail.com>
2019-06-26 18:21:00 +0300

    Multiscreen App

--- TestProject/TestProject.Droid/MainActivity.cs
+++ TestProject/TestProject.Droid/MainActivity.cs
@@ -12,16 +12,34 @@ namespace TestProject.Droid
     [Activity(Label = "options Menu", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        private Button _signUp;
+        private Button _submitNewUser;
+        private EditText _txtUserName;
+        private EditText _txtEmail;
+        private EditText _txtPassword;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
-            GridView gridView = FindViewById<GridView>(Resource.Id.gridView1);
-            gridView.Adapter = new ImageAdapter(this);
-            gridView.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs e)
+            _signUp = FindViewById<Button>(Resource.Id.btnSignUp);
+            _submitNewUser = FindViewById<Button>(Resource.Id.btnSave);
+            _txtUserName = FindViewById<EditText>(Resource.Id.txtUsername);
+            _txtEmail = FindViewById<EditText>(Resource.Id.txtEmail);
+            _txtPassword = FindViewById<EditText>(Resource.Id.txtPassword);
+
+            _signUp.Click += (object sender, EventArgs e) =>
               {
-                  Toast.MakeText(this, (e.Position + 1).ToString(), ToastLength.Short).Show();
+                  FragmentTransaction transFrags = FragmentManager.BeginTransaction();
+                  SignUpDialog signUpDialog = new SignUpDialog();
+                  signUpDialog.Show(transFrags, "Fragment Dialog");
+                  signUpDialog._onSignUpComplete += SignUpDialog_onSignUpComplete;
               };
+        }
+
+        private void SignUpDialog_onSignUpComplete(object sender, SignUpEventArgs e)
+        {
+            StartActivity(typeof(UserRegisteredActivity));
         }
     }
 }
---
+++ TestProject/TestProject.Droid/SignUpEventArgs.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TestProject.Droid
+{
+    class SignUpEventArgs : EventArgs
+    {
+        private string _userName;
+        private string _email;
+        private string _password;
+
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                _userName = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+
+        public string PassWord
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+            }
+        }
+
+        public SignUpEventArgs(string username, string email, string password)
+            :base()
+        {
+            _userName = username;
+            _email = email;
+            _password = password;
+        }
+    }
+}
