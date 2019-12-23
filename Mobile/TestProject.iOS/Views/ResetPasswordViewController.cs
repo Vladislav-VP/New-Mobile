def13a80572b244aee825fe2035f1eab86262ee5
Владислав Пархоменко <vladislav.parkhomenko@yandex.ua>
2019-12-23 09:36:13 +0200

    temp commit (password reset on iOS)

---
+++ Mobile/TestProject.iOS/Views/ResetPasswordViewController.cs
@@ -0,0 +1,40 @@
+﻿using MvvmCross.Binding.BindingContext;
+using MvvmCross.Plugin.Color.Platforms.Ios;
+using UIKit;
+
+using TestProject.Core.ViewModels;
+using TestProject.Resources;
+using MvvmCross.Platforms.Ios.Presenters.Attributes;
+
+namespace TestProject.iOS.Views
+{
+    [MvxChildPresentation]
+    public partial class ResetPasswordViewController : BaseViewController<ResetPasswordViewModel>
+    {
+        public override void InitializeAllControls()
+        {
+            Title = Strings.RegistrationTitle;
+            NavigationController.NavigationBar.BarStyle = UIBarStyle.Black;
+            UINavigationBar.Appearance.BarTintColor = AppColors.DarkBlue.ToNativeColor();
+            NavigationController.SetNavigationBarHidden(false, true);
+
+            lbUsername.Text = Strings.UsernameTextViewLabel;
+            lbEmail.Text = Strings.EmailLabel;
+            lbPassword.Text = Strings.PasswordTextViewLabel;
+            btRegistration.SetTitle(Strings.RegistrationButtonLabel, UIControlState.Normal);
+            tfPassword.SecureTextEntry = true;
+        }
+
+        public override void CreateBindings()
+        {
+            var set = this.CreateBindingSet<ResetPasswordViewController, ResetPasswordViewModel>();
+
+            //set.Bind(tfUsername).To(vm => vm.UserName);
+            //set.Bind(tfEmail).To(vm => vm.Email);
+            //set.Bind(tfPassword).To(vm => vm.Password);
+            //set.Bind(btRegistration).To(vm => vm.RegisterUserCommand);
+
+            set.Apply();
+        }
+    }
+}
---
+++ Mobile/TestProject.iOS/Views/ResetPasswordViewController.designer.cs
@@ -0,0 +1,91 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace TestProject.iOS.Views
+{
+    [Register ("ResetPasswordViewController")]
+    partial class ResetPasswordViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btRegistration { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lbEmail { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lbPassword { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lbUsername { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField tfEmail { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField tfPassword { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField tfUsername { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView vUserInfo { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btRegistration != null) {
+                btRegistration.Dispose ();
+                btRegistration = null;
+            }
+
+            if (lbEmail != null) {
+                lbEmail.Dispose ();
+                lbEmail = null;
+            }
+
+            if (lbPassword != null) {
+                lbPassword.Dispose ();
+                lbPassword = null;
+            }
+
+            if (lbUsername != null) {
+                lbUsername.Dispose ();
+                lbUsername = null;
+            }
+
+            if (tfEmail != null) {
+                tfEmail.Dispose ();
+                tfEmail = null;
+            }
+
+            if (tfPassword != null) {
+                tfPassword.Dispose ();
+                tfPassword = null;
+            }
+
+            if (tfUsername != null) {
+                tfUsername.Dispose ();
+                tfUsername = null;
+            }
+
+            if (vUserInfo != null) {
+                vUserInfo.Dispose ();
+                vUserInfo = null;
+            }
+        }
+    }
+}
